7525acc0ff26595e29c0938e9e571d12d0705715
Eyimofe Cole <153179802+EyimofeGitHub@users.noreply.github.com>
2026-09-21 22:26:38 +0100

    Add files via upload

---
+++ ItemShop.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Keisus_Realm;
+
+/**
+ *
+ * @author eyimo
+ */
+public class ItemShop {
+/*The item shop will have up to 3 things in the shop
+such as a better weapon
+Health potions
+and Armour that can affect attributs (essentially swapping DEF for ATK or ATK for HP or HP or DEF or just overall improve different stats and decrease others)
+*/
+}
